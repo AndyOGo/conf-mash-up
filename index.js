94a7b528fcf876a3cb04551f86971e4a0408a3c5
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-03-13 11:20:51 +0100

    added entry file

---
+++ index.js
@@ -0,0 +1,24 @@
+var defaultOptions = {
+    mergeDefaultConfig: true
+};
+var confLoader = require('./lib/confLoader');
+
+function mashConf(options, done) {
+    confLoader.load(options, function (err, config) {
+        if(err) {
+            return done(err);
+        }
+
+        done(null, config);
+    });
+}
+
+function mashConfSync(options) {
+    confLoader.loadSync(options);
+}
+
+module.exports = {
+    mashConf: mashConf,
+    mashConfSync: mashConfSync
+};
+
